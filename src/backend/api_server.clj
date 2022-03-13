fbdb1598fa5db42ef13f6425372fa8396a38661d
Ivan Shamshurin <shashmurin.i@gazgolder.ru>
2022-03-13 09:20:14 +0300

    logic for start API server

--- src/backend/api_server.clj
+++ src/backend/api_server.clj
@@ -2,6 +2,8 @@
   (:gen-class)
   (:require [org.httpkit.server :as server]))
 
+(defonce ^:private api-server (atom nil))
+
 (defn handler
   "A function that handles all requests from the server.
   Arguments: `req` is a ring request hash-map
@@ -11,8 +13,17 @@
    :headers {}
    :body    "Hello Clojure Server world!"})
 
-(defn create-server
-  "A ring-based server listening to all http requests
-  port is an Integer greater than 128"
-  [port]
-  (server/run-server handler {:port port}))
+(defn stop-server
+  "Stop httpkit server"
+  []
+  (when-not (nil? @api-server)
+    (@api-server :timeout 100)
+    (reset! api-server nil)))
+
+(defn -main
+  "Start a httpkit server"
+  [& {:keys [ip port]
+      :or   {ip   "0.0.0.0"
+             port 8000}}]
+  (println "INFO: Starting httpkit server on port:" port)
+  (reset! api-server (server/run-server #'handler {:port port})))
